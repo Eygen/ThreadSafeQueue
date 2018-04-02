7640ebf489f78a89ce9b8d5054e186e40a9892ff
Eygen <zhanul.88@gmail.com>
2018-04-02 21:24:17 +0600

    Add project files.

---
+++ ThreadSafeQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThreadSafeQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
