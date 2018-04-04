36700bf70bbdf93d1a6652ff24d53354ba4c51c9
Eygen <zhanul.88@gmail.com>
2018-04-05 01:15:16 +0600

    Add exampe for queue's using

--- ThreadSafeQueue/Program.cs
+++ ThreadSafeQueue/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Threading;
 
 namespace ThreadSafeQueue
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+  class Program
+  {
+    static TestQueue<int> queue = new TestQueue<int>();
+
+    static void Main(string[] args)
+    {                        
+      Thread popThread = new Thread(new ThreadStart(Dequeue));
+      popThread.Start();      
+      for (var i = 1; i <= 10; i++)
+      {
+        queue.Push(i);
+      }  
+      
+      foreach (int item in queue)
+      {
+        Console.WriteLine(item);
+      }
+      Dequeue();
+      Console.ReadKey();
     }
+
+    public static void Dequeue()
+    {
+      int popNumber = queue.Pop();
+      Console.WriteLine($"Извлеченный элемент: {popNumber}");
+    }
+  }
 }
